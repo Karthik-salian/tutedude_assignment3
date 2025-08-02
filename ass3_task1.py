3e296bf82cbae7ac462358ecf691482cbe251fec
Karthik-salian <143799755+Karthik-salian@users.noreply.github.com>
2025-08-02 10:00:12 +0530

    Add files via upload

---
+++ ass3_task1.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    if n<2:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+num=int(input("Enter a number: "))
+fact=factorial(num)
+print("Factorial of",num,"is:",fact)
